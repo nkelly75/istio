8addc6028c4550fccf4e3eb9dc9be15db20ddee0
Niall Kelly <nkelly75@gmail.com>
2018-06-27 20:13:59 +0100

    interim progress on websockets with tickers

--- addons/servicegraph/cmd/server/main.go
+++ addons/servicegraph/cmd/server/main.go
@@ -17,15 +17,25 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"html/template"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"time"
+
+	"github.com/gorilla/websocket"
 
 	"istio.io/istio/addons/servicegraph"
 	"istio.io/istio/addons/servicegraph/dot"
 	"istio.io/istio/addons/servicegraph/promgen"
 )
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true;
+	},
+}
 
 func writeJSON(w io.Writer, g *servicegraph.Dynamic) error {
 	return json.NewEncoder(w).Encode(g)
@@ -72,6 +82,69 @@ type state struct {
 	staticGraph *servicegraph.Static
 }
 
+func echo(w http.ResponseWriter, r *http.Request) {
+
+	log.Print("In echo")
+
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
+
+func vizSocket(w http.ResponseWriter, r *http.Request) {
+
+	log.Print("In vizSocket")
+
+	tickChan := time.NewTicker(time.Second * 2).C
+
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	doneChan := make(chan bool)
+	defer func() {
+		log.Print("in defer", err)
+		c.Close()
+		doneChan <- true
+	}()
+	for {
+		select {
+		case <- tickChan:
+			log.Print("Ticker ticked");
+			err = c.WriteMessage(websocket.TextMessage, []byte("NGK.."))
+			if err != nil {
+				log.Println("write:", err)
+				break
+			}
+		case <- doneChan:
+			log.Print("Done");
+			return
+		}
+		log.Print("After select")
+	}
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	homeTemplate.Execute(w, "ws://"+r.Host+"/echo")
+}
+
 func main() {
 	bindAddr := flag.String("bindAddr", ":8088", "Address to bind to for serving")
 	promAddr := flag.String("prometheusAddr", "http://localhost:9090", "Address of prometheus instance for graph generation")
@@ -92,7 +165,89 @@ func main() {
 	http.Handle("/vizgraph1", promgen.NewPromHandler(*promAddr, s.staticGraph, servicegraph.GenerateVizJSON1))
 	http.Handle("/vizgraph2", promgen.NewPromHandler(*promAddr, s.staticGraph, servicegraph.GenerateVizJSON2))
 	http.Handle("/vizgraph3", promgen.NewPromHandler(*promAddr, s.staticGraph, servicegraph.GenerateVizJSON3))
+	http.HandleFunc("/echo", echo)
+	http.HandleFunc("/h", home)
+	http.HandleFunc("/vizSocket", vizSocket)
 
 	log.Printf("Starting servicegraph service at %s", *bindAddr)
 	log.Fatal(http.ListenAndServe(*bindAddr, nil))
 }
+
+var homeTemplate = template.Must(template.New("").Parse(`
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<script>
+window.addEventListener("load", function(evt) {
+
+    var output = document.getElementById("output");
+    var input = document.getElementById("input");
+    var ws;
+
+    var print = function(message) {
+        var d = document.createElement("div");
+        d.innerHTML = message;
+        output.appendChild(d);
+    };
+
+    document.getElementById("open").onclick = function(evt) {
+        if (ws) {
+            return false;
+        }
+        ws = new WebSocket("{{.}}");
+        ws.onopen = function(evt) {
+            print("OPEN");
+        }
+        ws.onclose = function(evt) {
+            print("CLOSE");
+            ws = null;
+        }
+        ws.onmessage = function(evt) {
+            print("RESPONSE: " + evt.data);
+        }
+        ws.onerror = function(evt) {
+            print("ERROR: " + evt.data);
+        }
+        return false;
+    };
+
+    document.getElementById("send").onclick = function(evt) {
+        if (!ws) {
+            return false;
+        }
+        print("SEND: " + input.value);
+        ws.send(input.value);
+        return false;
+    };
+
+    document.getElementById("close").onclick = function(evt) {
+        if (!ws) {
+            return false;
+        }
+        ws.close();
+        return false;
+    };
+
+});
+</script>
+</head>
+<body>
+<table>
+<tr><td valign="top" width="50%">
+<p>Click "Open" to create a connection to the server,
+"Send" to send a message to the server and "Close" to close the connection.
+You can change the message and send multiple times.
+<p>
+<form>
+<button id="open">Open</button>
+<button id="close">Close</button>
+<p><input id="input" type="text" value="Hello world!">
+<button id="send">Send</button>
+</form>
+</td><td valign="top" width="50%">
+<div id="output"></div>
+</td></tr></table>
+</body>
+</html>
+`))
